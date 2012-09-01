08ac782d57b2417b4dcb6eb89c7b85dc29185b65
talam001 <talam@hotmail.com>
2012-08-31 22:07:44 -0400

    Adding file

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public String Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        public string Browse(string genre)
+        {
+            return HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+        }
+
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+
+    }
+}
